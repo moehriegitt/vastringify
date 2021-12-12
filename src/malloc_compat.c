601f6778c30a219b35d04c41ae6faadac24bae6b
moehriegitt <github@ndwun.de>
2021-12-12 13:18:33 +0100

    add missing file

---
+++ src/malloc_compat.c
@@ -0,0 +1,34 @@
+/* -*- Mode: C -*- */
+/* (c) Henrik Theiling, LICENSE: BSD-3-Clause */
+
+#include <assert.h>
+#include <stdlib.h>
+#include "va_print/malloc.h"
+
+/* ********************************************************************** */
+/* extern functions */
+
+extern void *va_alloc(
+    void *data,
+    size_t nmemb,
+    size_t size)
+{
+    assert(size != 0);
+    if (nmemb == 0) {
+        free(data);
+        return NULL;
+    }
+
+#if 0
+    size_t total = nmemb * size;
+    if ((total / size) != nmemb) {
+        return NULL;
+    }
+#else
+    size_t total;
+    if (__builtin_mul_overflow(nmemb, size, &total)) {
+        return NULL;
+    }
+#endif
+    return realloc(data, total);
+}
